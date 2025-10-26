439c26440d064beab13fde29839a11e000f134a9
Christian Andrei Santiago <christiansantiago@gmail.com>
2025-10-26 19:45:49 +0800

    responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,10 +7,8 @@ export default function NavBar() {
         <img src="/images/logo.svg" alt="CodePlay Caramel" style={{height:36}} />
       </div>
       <ul className="cp-navbar__links">
-        <li><a href="#">Lexer</a></li>
-        <li><a href="#">Parser</a></li>
-        <li><a href="#">Syntax</a></li>
-        <li><a href="#">Semantic</a></li>
+        <li><a href="#">Analyser</a></li>
+        <li><a href="#">About</a></li>
       </ul>
     </nav>
   );
