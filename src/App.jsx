fafcd9c70e7f07b5b82806b533e554f652e462da
Christian Andrei Santiago <christiansantiago@gmail.com>
2025-10-26 14:24:53 +0800

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { tokenize } from "./lexer";
 import "./styles.css";
+import NavBar from "./components/NavBar";
+import "./components/NavBar.css";
 
 export default function App() {
   const [code, setCode] = useState(
@@ -19,55 +21,59 @@ thread("hello world");`
   };
 
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {/* Left: code editor */}
-      <div className="editor">
-        <h3>Code Editor</h3>
-        <textarea
-          className="textarea"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-        />
-        <button className="tokenize-btn" onClick={handleTokenize}>
-          Tokenize
-        </button>
-      </div>
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <NavBar />
 
-      {/* Right: tokens */}
-      <div className="tokens">
-        <h3>Tokens</h3>
-        {hasRun ? (
-          <>
-            <table>
-              <thead>
-                <tr>
-                  <th>Type</th>
-                  <th>Lexeme</th>
-                  <th>Line</th>
-                  <th>Column</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tokens.map((t, i) => (
-                  <tr key={i}>
-                    <td>{t.type}</td>
-                    <td>{t.lexeme}</td>
-                    <td>{t.line}</td>
-                    <td>{t.column}</td>
+      <div style={{ display: "flex", flex: 1 }}>
+        {/* Left: code editor */}
+        <div className="editor">
+          <h3>Code Editor</h3>
+          <textarea
+            className="textarea"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <button className="tokenize-btn" onClick={handleTokenize}>
+            Tokenize
+          </button>
+        </div>
+
+        {/* Right: tokens */}
+        <div className="tokens">
+          <h3>Tokens</h3>
+          {hasRun ? (
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>Lexeme</th>
+                    <th>Line</th>
+                    <th>Column</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {tokens.map((t, i) => (
+                    <tr key={i}>
+                      <td>{t.type}</td>
+                      <td>{t.lexeme}</td>
+                      <td>{t.line}</td>
+                      <td>{t.column}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
-            <div className="error-box">
-              {tokens.some((t) => t.type === "ERROR")
-                ? "Lexical error found!"
-                : "No errors"}
-            </div>
-          </>
-        ) : (
-          <p>Press “Tokenize” to start.</p>
-        )}
+              <div className="error-box">
+                {tokens.some((t) => t.type === "ERROR")
+                  ? "Lexical error found!"
+                  : "No errors"}
+              </div>
+            </>
+          ) : (
+            <p>Press “Tokenize” to start.</p>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="cp-navbar">
+      <div className="cp-navbar__brand">
+        <img src="/images/logo.png" alt="CodePlay Caramel" style={{height:36}} />
+      </div>
+      <ul className="cp-navbar__links">
+        <li><a href="#">Lexer</a></li>
+        <li><a href="#">Parser</a></li>
+        <li><a href="#">Syntax</a></li>
+        <li><a href="#">Semantic</a></li>
+      </ul>
+    </nav>
+  );
+}
