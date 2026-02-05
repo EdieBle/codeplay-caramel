2e589bb04f55d816dfbd8fe05b3a268303558ac8
Pixielz <trishamae27568@gmail.com>
2026-02-05 23:11:22 +0800

    introduced a new prod: case_lit

--- src/Lexer/atomDelim.py
+++ src/Lexer/atomDelim.py
@@ -85,7 +85,7 @@ DELIM_VAL = {
     "spacebraces_delim": list(set(space_delim + ['{'])),
     "spaceparen_delim": list(set(space_delim + ['('])),
     "string_delim": list(set(space_delim + [')', ']'] + newline + relational_op + ['+', ','])),
-    "temp_delim": list(set(space_delim + newline + relational_op + [',', '&', '|'])),
+    "temp_delim": list(set(space_delim + newline + relational_op + [',', '&', '|', ':'])),
     "unary_delim": list(set(space_delim + newline + alpha_small + [')'])) # + ['1','2','3','4','5','6','7','8','9'] (removed these for now cuz a unary being delimited by a num makes no sense 4:17am)
 }
 KEYWORDS_TABLE = {
--- src/Lexer/transitionDiag.py
+++ src/Lexer/transitionDiag.py
@@ -192,7 +192,7 @@ TRANSITIONS_DFA = {
             293: State('\'', 294), 
 
             # delimiter
-            294: State([*DELIM_VAL["space_delim"], ',', '\n', ']', ')'], end=True, token_type="CHURROLIT"),
+            294: State([*DELIM_VAL["space_delim"], ',', '\n', ']', ')', ':'], end=True, token_type="CHURROLIT"),
 
             # Escape sequence
             295: State('\\', 296), 296: State(ATOMIC_VAL["escapeseq_let"], [293]),
